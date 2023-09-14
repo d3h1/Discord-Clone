4cc4fc8cd309f08e424a4ef6595bb0f95b4eb186
d3h1 <denicabaravdic@gmail.com>
2023-09-14 14:06:24 -0700

    image upload showing image

--- discord-clone/components/file-upload.tsx
+++ discord-clone/components/file-upload.tsx
@@ -1,9 +1,34 @@
 "use client";
 
-export const FileUpload = () => {
-    return (
-        <div>
-            File Upload Component
-        </div>
-    )
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css"
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
 }
+
+export const FileUpload = ({
+    onChange,
+    value,
+    endpoint
+} : FileUploadProps) => {
+    const fileType = value?.split
+            
+    return (
+        <UploadDropzone
+            endpoint={endpoint}
+            onClientUploadComplete={(res) => {
+                onChange(res?.[0].url);
+            }}
+            onUploadError={(error: Error) => {
+                console.log(error)
+            }}
+        />
+    );
+};
