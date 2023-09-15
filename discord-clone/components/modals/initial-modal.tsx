16c87f86a96951abbb6a375509903d2e025ad50e
d3h1 <denicabaravdic@gmail.com>
2023-09-15 16:08:10 -0700

    working on server connection

--- discord-clone/components/modals/initial-modal.tsx
+++ discord-clone/components/modals/initial-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios"
 import * as z from "zod";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -27,6 +28,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -39,6 +41,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     setIsMounted(true);
@@ -55,8 +59,16 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-  };
+    try {
+      await axios.post("/api/servers", values);
+
+      form.reset();
+      router.refresh();
+      window.location.reload()
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   if (!isMounted) {
     return null;
